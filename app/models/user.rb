17dcb3acd9c9df069ebdc412f986e6d55fc5f296
mshr-Hub <masa.megane.616@gmail.com>
2019-06-28 18:50:32 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :group_uses
+  has_many :groups, through: :group_uses
 end
