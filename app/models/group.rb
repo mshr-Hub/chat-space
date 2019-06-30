dbd07231043049cf64abd93f535d4dc5136b5473
mshr-Hub <masa.megane.616@gmail.com>
2019-06-30 17:52:10 +0900

    Update group.rb

--- app/models/group.rb
+++ app/models/group.rb
@@ -7,9 +7,13 @@ class Group < ApplicationRecord
 
   def show_last_message
     if (last_message = message.last).present?
-    
+      if last_message.content?
+        last_message.content
+      else
+        '画像が投稿されています'
+      end
     else
       'まだメッセージはありません。'
     end
-
+  end
 end
