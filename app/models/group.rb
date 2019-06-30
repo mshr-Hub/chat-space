433d189fda70e91b3f20c94b5683278124947f44
mshr-Hub <masa.megane.616@gmail.com>
2019-06-30 19:05:38 +0900

    Update group.rb

--- app/models/group.rb
+++ app/models/group.rb
@@ -6,7 +6,7 @@ class Group < ApplicationRecord
   validates :name, presence: true, uniqueness: true
 
   def show_last_message
-    if (last_message = message.last).present?
+    if (last_message = messages.last).present?
       if last_message.content?
         last_message.content
       else
