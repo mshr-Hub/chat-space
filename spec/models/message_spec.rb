fdb637ecafafc2b7009cee7e510868c3a22a1d97
mshr-Hub <masa.megane.616@gmail.com>
2019-07-08 23:39:09 +0900

    Update message_spec.rb

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -18,6 +18,23 @@ RSpec.describe Message, type: :model do
     end
 
     context "can not save" do
+      It "is invalid without content and image" do
+        message = build(:message, content: nil, image: nil)
+        message.valid?
+        expect(message.errors[:content]).to include("can't be blank")
+      end
+
+      It "is invalid without group_id" do
+        message = build(:message, group_id: nil)
+        message.valid?
+        expect(message.errors[:group]).to include("can't be blank")
+      end
+
+      It "is invalid without user_id" do
+        message = build(:message, user_id: nil)
+        message.valid?
+        expect(message.errors[:user]).to include("can't be blank")
+      end
     end
 
   end
