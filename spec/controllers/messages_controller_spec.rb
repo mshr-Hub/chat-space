b347c9555e9d89c2a84f7badbbc270b85c82cf5a
mshr-Hub <masa.megane.616@gmail.com>
2019-07-09 21:24:29 +0900

    Update messages_controller_spec.rb

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -29,6 +29,10 @@ describe MessagesController do
       before do
         get :index, params: { group_id: group.id }
       end
+
+      it "redirects to new_user_session_path" do
+        expect(response).to redirect_to(new_user_session_path)
+      end
     end
 
   end
