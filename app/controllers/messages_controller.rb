d95c36ff341102aef1076d433779bb4a06302895
mshr-Hub <masa.megane.616@gmail.com>
2019-07-16 02:20:57 +0900

    Update messages_controller.rb

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -8,6 +8,7 @@ class MessagesController < ApplicationController
 
   def create
     @message = @group.messages.new(message_params)
+    @message.save
     respond_to do |format|
       format.html
       format.json
