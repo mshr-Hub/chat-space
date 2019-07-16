16069ca9af04f53f5782d40d75566450235508d0
mshr-Hub <masa.megane.616@gmail.com>
2019-07-16 22:00:03 +0900

    Update message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,23 @@
 $(function() {
+  function buildHTML(message) {
+    var content = message.content ? `${message.content}` : "";
+    var img = message.image ? `<img src=${message.image}>` : "";
+    var html = `<div class="message">
+                  <div class="message__upper-info">
+                    <p class="message__upper-info__talker">
+                      ${message.user_name}
+                    </p>
+                    <p class="message__upper-info__date">
+                      ${message.date}
+                    </p>
+                  </div>
+                  <p class="message__text">
+                    ${content}
+                    ${img}
+                  </p>
+                </div>`
+    return html;
+  }
   $('#new_message').on("submit", function(e) {
     e.preventDefault();
     var formData = new FormData(this);
@@ -11,5 +30,13 @@ $(function() {
       processData: false,
       contentType: false
     })
-  });
+    .done(function(data) {
+      var html = buildHTML(data);
+      $('.messages').append(html);
+      $('#message_content').val('');
+    })
+    .fail(function() {
+      alert('メッセージの送信に失敗しました');
+    })
+  })
 });
