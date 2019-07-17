be018a83613fe62c07ac8e1be57716d71d9427c7
mshr-Hub <masa.megane.616@gmail.com>
2019-07-18 01:37:26 +0900

    Update user_search.js

--- app/assets/javascripts/user_search.js
+++ app/assets/javascripts/user_search.js
@@ -1,4 +1,15 @@
 $(function() {
+
+  var user_search_result = $('#user-search-result');
+
+  function appendUser(user) {
+    var html = `<div class="chat-group-user clearfix">
+                  <p class="chat-group-user__name">${user.name}</p>
+                  <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
+                </div>`
+    user_search_result.append(html);
+  }
+
   $('#user-search-field').on("keyup", function() {
     var input = $('#user-search-field').val();
     $.ajax({
@@ -7,5 +18,19 @@ $(function() {
       data: { keyword: input },
       dataType: 'json'
     })
-  })
+    .done(function(users) {
+      $("#user-search-result").empty();
+      if (users.length !== 0) {
+        users.forEach(function(user) {
+          appendUser(user);
+        });
+      }
+      else {
+        appendErrMsgToHTML("一致するユーザーが存在しません");
+      }
+    })
+    .fail(function() {
+      alert('ユーザー検索に失敗しました');
+    })
+  });
 });
