18dbe29b831a96277a1fe64cdaca9d07d74d65d3
mshr-Hub <masa.megane.616@gmail.com>
2019-07-18 01:14:49 +0900

    Update user_search.js

--- app/assets/javascripts/user_search.js
+++ app/assets/javascripts/user_search.js
@@ -1,6 +1,11 @@
 $(function() {
   $('#user-search-field').on("keyup", function() {
     var input = $('#user-search-field').val();
-    console.log(input);
+    $.ajax({
+      type: 'GET',
+      url: '/users/index',
+      data: { keyword: input },
+      dataType: 'json'
+    })
   })
 });
