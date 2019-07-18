2e9a6908a66c18c006f774b9a725fbe4627aa8eb
mshr-Hub <masa.megane.616@gmail.com>
2019-07-18 23:16:36 +0900

    Update user_search.js

--- app/assets/javascripts/user_search.js
+++ app/assets/javascripts/user_search.js
@@ -18,10 +18,10 @@ $(function() {
   }
   function appendChatUser(user) {
     var html = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
-                <input name='group[user_ids][]' type='hidden' value='${user.id}'>
-                <p class='chat-group-user__name'>${user.name}</p>
-                <div class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</div>
-              </div>`
+                  <input name='group[user_ids][]' type='hidden' value='${user.id}'>
+                  <p class='chat-group-user__name'>${user.name}</p>
+                  <div class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</div>
+                </div>`
   chat_group_users.append(html);
   }
 
