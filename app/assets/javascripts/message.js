df6d6b18e2fe3cc9e544dbf37df8ed75bffc0c5a
mshr-Hub <masa.megane.616@gmail.com>
2019-07-20 01:16:21 +0900

    Update message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -18,6 +18,57 @@ $(function() {
                 </div>`
     return html;
   }
+  var buildMessageHTML = function(message) {
+    if (message.content && message.image.url) {
+      var html = `<div class="message" data-id="${message.id}">
+                    <div class="upper-message">
+                      <div class="upper-message__user-name">
+                        ${message.user_name}
+                      </div>
+                      <div class="upper-message__date">
+                        ${message.date}
+                      </div>
+                    </div>
+                    <div class="lower-message">
+                      <p class="lower-message__content">
+                        ${message.content}
+                      </p>
+                      <img src="${message.image.url}" class="lower-message__image">
+                    </div>
+                  </div>`
+    } else if (message.content) {
+      var html = `<div class="message" data-id=${message.id}>
+                    <div class="upper-message">
+                      <div class="upper-message__user-name">
+                        ${message.user_name}
+                      </div>
+                      <div class="upper-message__date">
+                        ${message.date}
+                      </div>
+                    </div>
+                    <div class="lower-message">
+                      <p class="lower-message__content">
+                        ${message.content}
+                      </p>
+                    </div>
+                  </div>`
+    } else if (message.image.url) {
+      var html = `<div class="message" data-id=${message.id}>
+                    <div class="upper-message">
+                      <div class="upper-message__user-name">
+                        ${message.user_name}
+                      </div>
+                      <div class="upper-message__date">
+                        ${message.date}
+                      </div>
+                    </div>
+                    <div class="lower-message">
+                      <img src="${message.image.url}" class="lower-message__image">
+                    </div>
+                  </div>`
+    };
+    return html;
+  };
   function scrollBottom() {
     var target = $('.message').last();
     var position = target.offset().top + $('.messages').scrollTop();
