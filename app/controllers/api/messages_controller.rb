02bdceecf83973f546ce0b8813a492a8f7938abc
mshr-Hub <masa.megane.616@gmail.com>
2019-07-20 15:34:40 +0900

    Update messages_controller.rb

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,5 +1,5 @@
 class Api::MessagesController < ApplicationController
   def index
-    @messages = Message.where('id > ?', params[:id])
+    @messages = Message.where('id > ?', "#{params[:id]}").where(group_id: params[:group_id])
   end
 end
